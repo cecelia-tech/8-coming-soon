cf74e79a4e0f67c158fe23a75da3eb9e01038bb3
cecelia-tech <79083749+cecelia-tech@users.noreply.github.com>
2021-03-28 16:40:12 +0300

    Folders created

---
+++ js/main-home.js
@@ -0,0 +1,14 @@
+//IMPORT
+//hero
+//main
+//footer
+
+//FUNKCIJU NAUDOJIMAS
+//hero
+//main
+//footer
+
+import {suma} from './components/sum.js';
+import {divide} from './components/divide.js';
+console.log(suma(2, 3));
+console.log(divide(6, 3));
